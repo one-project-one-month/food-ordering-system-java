8cc05f22d1c2891732b3fd5889cfdb5bd5d08b8d
thanthtooaung <thanthtoo1285@gmail.com>
2025-07-02 13:34:22 +0700

    initial commit

---
+++ src/test/java/org/_p1m/foodorderingsystem/FoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org._p1m.foodorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
