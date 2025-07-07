93d255830f18c18ee1ca13d52ca36b901fd64961
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-07-07 22:07:57 +0630

    add classes for login.signup.fp

---
+++ src/main/java/org/_p1m/foodorderingsystem/features/forgotPassword/controller/ForgotPasswordController.java
@@ -0,0 +1,4 @@
+package org._p1m.foodorderingsystem.features.forgotPassword.controller;
+
+public class ForgotPasswordController {
+}
---
+++ src/main/java/org/_p1m/foodorderingsystem/features/forgotPassword/service/ForgotPasswordService.java
@@ -0,0 +1,4 @@
+package org._p1m.foodorderingsystem.features.forgotPassword.service;
+
+public class ForgotPasswordService {
+}
