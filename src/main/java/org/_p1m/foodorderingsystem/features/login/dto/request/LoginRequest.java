57a3146f80fd2f29b57cf2819705289f3e3bc50d
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-07-07 11:58:56 +0630

    add for login signup

--- src/main/java/org/_p1m/foodorderingsystem/features/login/dto/request/LoginRequest.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/login/dto/request/LoginRequest.java
@@ -1,4 +1,9 @@
 package org._p1m.foodorderingsystem.features.login.dto.request;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    private String username;
+    private String password;
 }
--- src/main/java/org/_p1m/foodorderingsystem/features/login/dto/response/LoginResponseDto.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/login/dto/response/LoginResponseDto.java
@@ -1,4 +1,15 @@
 package org._p1m.foodorderingsystem.features.login.dto.response;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Data
 public class LoginResponseDto {
+    private long success;
+    private long code;
+    private ArrayList<Objects>meta;
+    private ArrayList<Objects>data;
+    private ArrayList<String>message;
 }
