03156e33bdae9957bd5bf6e24491d2724ba5b6e0
Hein Khant Phyoe <heinkhantphyoe2811@gmail.com>
2025-07-10 12:33:32 +0800

    add swagger

--- src/main/java/org/_p1m/foodorderingsystem/features/menu/controller/ManageMenuController.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/menu/controller/ManageMenuController.java
@@ -1,12 +1,17 @@
 package org._p1m.foodorderingsystem.features.menu.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org._p1m.foodorderingsystem.config.response.dto.ApiResponse;
 import org._p1m.foodorderingsystem.config.response.util.ResponseUtils;
 import org._p1m.foodorderingsystem.features.menu.dto.request.CreateMenuRequest;
 import org._p1m.foodorderingsystem.features.menu.service.ManageMenuService;
+import org._p1m.foodorderingsystem.features.users.dto.request.UserCreateRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("${api.base.path}/auth/menus")
+@Tag(name = "Menu API", description = "Endpoints for managing menus")
 public class ManageMenuController {
 
     private  final ManageMenuService manageMenuService;
@@ -24,6 +30,19 @@ public class ManageMenuController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create a new menu",
+            description = "create a new menu for restaurant",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Create Menu request",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = CreateMenuRequest.class))
+            ),
+            responses = {
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Menu created successfully"),
+                    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Invalid request")
+            }
+    )
     public ResponseEntity<ApiResponse> createMenu(@Valid @RequestBody CreateMenuRequest createMenuRequest, HttpServletRequest request){
         final ApiResponse response = this.manageMenuService.createMenu(createMenuRequest);
         return ResponseUtils.buildResponse(request, response);
