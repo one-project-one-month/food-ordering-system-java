be792c4961a285145325286bba40b83423b95ba4
Hein Khant Phyoe <heinkhantphyoe2811@gmail.com>
2025-07-10 23:47:11 +0800

    fix errors

--- src/main/java/org/_p1m/foodorderingsystem/features/restaurant/service/RestaurantRegistrationService.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/restaurant/service/RestaurantRegistrationService.java
@@ -1,9 +1,10 @@
-package org._p1m.foodorderingsystem.features.restaurant.srevice;
+package org._p1m.foodorderingsystem.features.restaurant.service;
 
 import org._p1m.foodorderingsystem.config.response.dto.ApiResponse;
 import org._p1m.foodorderingsystem.features.restaurant.dto.request.RestaurantCreateRequest;
 import org._p1m.foodorderingsystem.features.restaurant.dto.request.RestaurantUpdateRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public interface RestaurantRegistrationService {
@@ -14,4 +15,6 @@ public interface RestaurantRegistrationService {
     ApiResponse deleteRestaurant(Long id);
 
     ApiResponse updateRestaurant(Long id, RestaurantUpdateRequest restaurantUpdateRequest);
+
+    String uploadRestaurantPicture(Long restaurantId, MultipartFile file);
 }
