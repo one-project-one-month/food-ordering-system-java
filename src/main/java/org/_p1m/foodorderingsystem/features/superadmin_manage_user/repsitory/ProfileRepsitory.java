1f822f58ca9bf9775301453e7a4b2ecbcfe20098
CThu <tt4306391@gmail.com>
2025-07-10 22:12:55 +0700

    delete user from admin point of view

--- src/main/java/org/_p1m/foodorderingsystem/features/superadmin_manage_user/repsitory/ProfileRepsitory.java
+++
@@ -1,9 +0,0 @@
-package org._p1m.foodorderingsystem.features.superadmin_manage_user.repsitory;
-
-import org._p1m.foodorderingsystem.model.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepsitory extends JpaRepository<Profile,Long> {
-}
