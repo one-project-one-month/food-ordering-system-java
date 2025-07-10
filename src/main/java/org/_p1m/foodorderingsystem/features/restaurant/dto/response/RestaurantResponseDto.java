2ae61ba2c38db99a00e4f6a94eb9579b5a8b86e7
ilyasbham <ilyasbham1@gmail.com>
2025-07-10 20:46:47 +0300

    ft/019: Implement getAllRestaurants API and add Restaurant feature

---
+++ src/main/java/org/_p1m/foodorderingsystem/features/restaurant/dto/response/RestaurantResponseDto.java
@@ -0,0 +1,15 @@
+package org._p1m.foodorderingsystem.features.restaurant.dto.response;
+
+import lombok.Data;
+
+@Data
+public class RestaurantResponseDto {
+    private Long id;
+    private String restaurantName;
+    private String contactNumber;
+    private String restaurantImage;
+    private String nrc;
+    private String kpayNumber;
+    private String createdAt;
+    private String updatedAt;
+}
