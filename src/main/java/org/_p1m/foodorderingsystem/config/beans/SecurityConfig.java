1cb099ba7ce90153028f73ae750afad066a9697a
Hase <james342506@gmail.com>
2025-07-12 01:21:43 +0630

    User Service

--- src/main/java/org/_p1m/foodorderingsystem/config/beans/SecurityConfig.java
+++ src/main/java/org/_p1m/foodorderingsystem/config/beans/SecurityConfig.java
@@ -1,23 +1,57 @@
 package org._p1m.foodorderingsystem.config.beans;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailService;
+
     @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf-> csrf.disable()).
-                authorizeHttpRequests(auth->auth
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).
+                authorizeHttpRequests(auth -> auth
                         .requestMatchers(new AntPathRequestMatcher("/${api.base.path}/auth/**")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
-  }
+    }
 }
--- src/main/java/org/_p1m/foodorderingsystem/features/users/service/UserService.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/users/service/UserService.java
@@ -2,6 +2,7 @@ package org._p1m.foodorderingsystem.features.users.service;
 
 import org._p1m.foodorderingsystem.config.response.dto.ApiResponse;
 import org._p1m.foodorderingsystem.features.users.dto.request.UserCreateRequest;
+import org._p1m.foodorderingsystem.model.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,4 +12,6 @@ public interface UserService {
     public ApiResponse createUser(UserCreateRequest request);
 
     public String uploadProfilePicture(final Long userId, final MultipartFile file);
+
+    public String varifiedUser(User user);
 }
