3f0ade217a44ca0a2c18871cabc4da8b57e0ff3a
YeZawHlaing <yezawhlaing0@gmail.com>
2025-07-12 03:00:39 +0630

    authentication done!

--- src/main/java/org/_p1m/foodorderingsystem/config/beans/SecurityConfig.java
+++ src/main/java/org/_p1m/foodorderingsystem/config/beans/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org._p1m.foodorderingsystem.config.beans;
 
 import lombok.RequiredArgsConstructor;
+import org._p1m.foodorderingsystem.features.users.service.impl.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,10 +20,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-
+@EnableWebSecurity
 public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailService;
+//    private UserDetailServiceImpl userDetailService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -47,7 +50,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).
                 authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/${api.base.path}/auth/**")).permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults())
--- src/main/java/org/_p1m/foodorderingsystem/features/users/service/impl/UserDetailServiceImpl.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/users/service/impl/UserDetailServiceImpl.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public  class UserDetailServiceImpl implements UserDetailsService {
-    private  UserRepository userRepository;
+    private final UserRepository userRepository;
 
 
 
