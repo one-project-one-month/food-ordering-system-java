5756a146ad0e1b5ac1c6565528d4ef76aecccf30
hlanhtetkyaw <hlan1559@gmail.com>
2025-07-14 23:31:09 +0630

    [FIX] profile schema and naming strategy

--- src/main/java/org/_p1m/foodorderingsystem/model/Role.java
+++ src/main/java/org/_p1m/foodorderingsystem/model/Role.java
@@ -1,16 +1,15 @@
 package org._p1m.foodorderingsystem.model;
 
+import org._p1m.foodorderingsystem.common.entity.MasterData;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org._p1m.foodorderingsystem.common.entity.MasterData;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "roles")
 public class Role extends MasterData {
     @Column(unique = true)
     private String name;
