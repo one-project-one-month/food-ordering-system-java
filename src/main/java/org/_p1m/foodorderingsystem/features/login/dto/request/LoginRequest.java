2bfba2301bf7e023a8803d62e7bd8f18b6941d51
ThetNaingPhone <thetnaingphone394@gmail.com>
2025-07-20 13:17:20 +0700

    Revert "Merge pull request #27 from one-project-one-month/ft/001_LoginSignUp"
    This reverts commit 4ab9a0e0f839921daa49b30ddb42e7cb34935016, reversing
    changes made to 29c0fe0a6320873108f8e438c3423c2e09a2be5d.

--- src/main/java/org/_p1m/foodorderingsystem/features/login/dto/request/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package org._p1m.foodorderingsystem.features.login.dto.request;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/org/_p1m/foodorderingsystem/features/login/service/impl/LoginServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package org._p1m.foodorderingsystem.features.login.service.impl;
-
-public class LoginServiceImpl {
-}
