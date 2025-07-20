281327290bcbb12234fe36aa91b088947ebd8996
hlanhtetkyaw <hlan1559@gmail.com>
2025-07-20 20:33:58 +0630

    [ADD] patch method

--- src/main/java/org/_p1m/foodorderingsystem/config/beans/SecurityConfig.java
+++ src/main/java/org/_p1m/foodorderingsystem/config/beans/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3001"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
