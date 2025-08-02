087407029329ce19c715c69a31c43ddbd946ac51
hlanhtetkyaw <hlan1559@gmail.com>
2025-08-03 00:55:28 +0630

    [ADD] delivery status change

--- src/main/java/org/_p1m/foodorderingsystem/features/delivery/service/DeliveryDataService.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/delivery/service/DeliveryDataService.java
@@ -6,6 +6,7 @@ import org._p1m.foodorderingsystem.config.response.dto.ApiResponse;
 import org._p1m.foodorderingsystem.config.response.dto.PaginatedApiResponse;
 import org._p1m.foodorderingsystem.features.delivery.dto.request.ApplyDeliveryStaffRequest;
 import org._p1m.foodorderingsystem.features.delivery.dto.request.AssignDeliveryRequest;
+import org._p1m.foodorderingsystem.features.delivery.dto.request.ChangeDeliveryStatusRequest;
 import org._p1m.foodorderingsystem.features.delivery.dto.response.GetAllAssignedDelivery;
 import org._p1m.foodorderingsystem.features.delivery.dto.response.GetAllVendorsResponseDto;
 import org.springframework.data.domain.Pageable;
@@ -20,4 +21,6 @@ public interface DeliveryDataService {
     ApiResponse applyRestaurantByDeliveryStaff(ApplyDeliveryStaffRequest request);
     
     PaginatedApiResponse<GetAllAssignedDelivery> getAllAssignedDelivery(Pageable pageable,Long restaurantId,DeliveryStatus status);
+    
+    ApiResponse changeDeliveryStatus(ChangeDeliveryStatusRequest changeDeliStatusReq);
 }
