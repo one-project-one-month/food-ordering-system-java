9f8d226c8a44ef2eafb3b1f741980c34a6008c3f
hlanhtetkyaw <hlan1559@gmail.com>
2025-08-03 19:09:24 +0630

    [ADD] get all addresses by user id

--- src/main/java/org/_p1m/foodorderingsystem/features/address/repository/AddressRepository.java
+++ src/main/java/org/_p1m/foodorderingsystem/features/address/repository/AddressRepository.java
@@ -4,8 +4,12 @@ package org._p1m.foodorderingsystem.features.address.repository;
 import org._p1m.foodorderingsystem.common.constant.AddressEntityType;
 import org._p1m.foodorderingsystem.common.constant.Status;
 import org._p1m.foodorderingsystem.model.Address;
+import org._p1m.foodorderingsystem.model.OrderData;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,6 +20,8 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
     @Query("SELECT a FROM Address a WHERE a.entityType = :entityType AND a.entityId = :entityId")
     Address findByEntityTypeAndEntityId(AddressEntityType entityType, Long entityId);
 
-
+    @Query("SELECT a FROM Address a WHERE a.entityId = :customerId")
+    Page<Address> findByEntityId(@Param("customerId") Long customerId, Pageable pageable);
+    
     Optional<Address> findByIdAndStatus(Long id, Status status);
 }
