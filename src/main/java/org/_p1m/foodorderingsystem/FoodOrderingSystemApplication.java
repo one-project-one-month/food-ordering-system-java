f68a78788085ed42e7f53d9320a1d5e9e6d917ce
Mintawlon <minhtawlon111@gmail.com>
2025-08-06 00:02:15 +0700

    change log

--- src/main/java/org/_p1m/foodorderingsystem/FoodOrderingSystemApplication.java
+++ src/main/java/org/_p1m/foodorderingsystem/FoodOrderingSystemApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
 public class FoodOrderingSystemApplication {
-
+	static {
+		Dotenv.configure().ignoreIfMissing().systemProperties().load();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FoodOrderingSystemApplication.class, args);
 	}
